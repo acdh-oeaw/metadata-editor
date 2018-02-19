9e0d05ec688dec6a4e8c8e848349f7d341b06909
Robin Kaggl <robin.kaggl@oeaw.ac.at>
2018-02-19 17:40:24 +0100

    It should not be possiple to upload any duplicates to the store now

--- src/store/modules/n3/actions.js
+++ src/store/modules/n3/actions.js
@@ -1,21 +1,31 @@
-/* eslint no-console: ["error", { allow: ["log"] }] */
+/* eslint no-console: ['error', { allow: ['log'] }] */
 
 const actions = {
+  AddFilteredTriples(state, triple) {
+    state.store.addTriple({
+      subject: actions.RemovePrefix(triple.subject),
+      predicate: actions.RemovePrefix(triple.predicate),
+      object: actions.RemovePrefix(triple.object),
+    });
+  },
   StringToStore({ state, commit }, string) {
-    let c = 0;
     commit('startProcessing', 'Loading File to Store...');
-    state.parser.parse(string, (error, triple, prefixes) => {
+    state.parser.parse(string, (error, triple) => {
       if (triple) {
-        state.store.addTriple(triple.subject, triple.predicate, triple.object);
-        console.log(triple);
-        c += 1;
+        actions.AddFilteredTriples(state, triple);
       } else {
         commit('updateTripleCount');
         commit('updateSubjects');
         commit('stopProcessing');
-        console.log(`Parsed ${c} triples to store.`, prefixes);
+        console.log(state.store.getTriples());
       }
     });
+  },
+  RemovePrefix(str) {
+    if (str.search(/b\d/) > -1) {
+      return str.replace(/b\d_/, '');
+    }
+    return str;
   },
 };
 
