7b1ac47508c9fe1af8a8c3d22b812675e3e3322a
Christoph Hoffmann <christoph.hoffmann@oeaw.ac.at>
2018-02-27 13:09:13 +0100

    minor

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -11,7 +11,7 @@ const CONFIG = {
       PLACES: 'places/',
       CONCEPTS: 'concepts/',
       PUBLICATIONS: 'publications/',
-      METADATA: '/getMetadata/'
+      METADATA: '/getMetadata/',
     },
     TIMEOUT: 1000,
     PARAMS: {
