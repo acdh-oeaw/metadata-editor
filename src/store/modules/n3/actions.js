f21e5daff8d0871e8f604e4a3aadfb1b96c2ae97
Christoph Hoffmann <christoph.hoffmann@oeaw.ac.at>
2018-02-27 12:31:54 +0100

    fixed use of helper methods

--- src/store/modules/n3/actions.js
+++ src/store/modules/n3/actions.js
@@ -1,20 +1,38 @@
 /* eslint no-console: ['error', { allow: ['log'] }] */
 
+
+// some helper functions iso mixin
+function RemovePrefix(str) {
+  if (str.search(/b\d/) > -1) {
+    return str.replace(/b\d_/, '');
+  }
+  return str;
+}
+
 const actions = {
-  AddFilteredTriples({ state, dispatch }, triple) {
+  AddTriple({ state, dispatch }, triple) {
     state.store.addTriple(
-      dispatch('RemovePrefix', triple.subject),
+      triple.subject,
       triple.predicate,
-      dispatch('RemovePrefix', triple.object),
+      triple.object,
     );
-    console.log(dispatch('RemovePrefix', triple.subject));
+  },
+  /* action to remove the prefixes n3.js automatically adds when parsing
+     blank namespaces before adding the triple
+     see http://rubenverborgh.github.io/N3.js/docs/N3Store.html#section-124 */
+  AddFilteredTriple({ state, dispatch }, triple) {
+    state.store.addTriple(
+      RemovePrefix(triple.subject),
+      triple.predicate,
+      RemovePrefix(triple.object),
+    );
   },
   StringToStore({ state, commit, dispatch }, string) {
     commit('startProcessing', 'Loading File to Store...');
     state.parser.parse(string, (error, triple) => {
       if (triple) {
         // console.log('a', triple);
-        dispatch('AddFilteredTriples', triple);
+        dispatch('AddFilteredTriple', triple);
         // actions.AddFilteredTriples(triple);
       } else {
         commit('updateTripleCount');
@@ -23,13 +41,6 @@ const actions = {
         console.log(state.store.getTriples());
       }
     });
-  },
-  RemovePrefix({ state }, str) {
-    console.log('a', str);
-    if (str.search(/b\d/) > -1) {
-      return str.replace(/b\d_/, '');
-    }
-    return str;
   },
 };
 
