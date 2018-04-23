ff7551c167d624f255cab210980e211044544b52
Christoph Hoffmann <christoph.hoffmann@oeaw.ac.at>
2018-04-23 12:52:49 +0200

    icons added

--- src/components/FundamentNav.vue
+++ src/components/FundamentNav.vue
@@ -1,16 +1,15 @@
 <template>
 <b-navbar toggleable="md" type="light" class="fixed-top bg-white box-shadow" v-if="menu">
   <div class="container">
-    <b-link class="navbar-brand" :to="{ name: 'start', params: { lang: 'en' }}">
+    <b-link class="navbar-brand" :to="menu.homestate">
       <img class="mr-2" :src="menu.logo" height="35px" alt="" />
       {{ menu.title }}
     </b-link>
     <b-navbar-toggle target="navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation"></b-navbar-toggle>
-
     <b-collapse is-nav id="navbarsExampleDefault">
       <b-navbar-nav>
           <b-nav-item class="nav-item active" v-for="item in menu.menu" :key="item.caption">
-            <b-link class="nav-link" :to="item.startpage">{{ item.caption }} <span class="sr-only">(current)</span></b-link>
+            <b-link class="nav-link" :to="item.startpage"><i :class="item.icon"></i> {{ item.caption }} <span class="sr-only">(current)</span></b-link>
           </b-nav-item>
       </b-navbar-nav>
       <div class="navbar-icons">
