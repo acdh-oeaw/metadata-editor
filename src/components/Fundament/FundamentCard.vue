13c6cc6e3f02efeebe5d54c87157c32cc538d67a
Christoph Hoffmann <christoph.hoffmann@oeaw.ac.at>
2018-04-30 13:56:45 +0200

    .

---
+++ src/components/Fundament/FundamentCard.vue
@@ -0,0 +1,49 @@
+<template>
+  <transition appear :duration="250" name="fadeLeft">
+    <v-card style="border-top: 5px solid #80cbc4!important;">
+      <v-card-media :src="src" v-if="src" height="200px">
+      </v-card-media>
+      <v-card-title primary-title>
+        <div>
+          <h3 class="headline mb-0" v-text="caption"></h3>
+          <slot></slot>
+        </div>
+      </v-card-title>
+      <v-card-actions>
+        <v-btn
+          flat
+          class="teak lighten-3"
+          :to="link"
+          >
+        More</v-btn>
+      </v-card-actions>
+    </v-card>
+  </transition>
+</template>
+
+<script>
+
+  export default {
+    data() {
+      return {
+      };
+    },
+    props: [
+      'src',
+      'caption',
+      'link',
+    ],
+    name: 'FundamentJumbo',
+  };
+</script>
+<style scoped>
+
+h3:after {
+  content: '';
+  display: block;
+  width: 50px;
+  border-top: 5px solid #80cbc4!important;
+  margin-top: 15px;
+  margin-bottom: 15px;
+}
+</style>
---
+++ src/components/Fundament/FundamentJumbo.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-parallax :src="src" height="250" class="mt-4 border-top">
+    <v-layout column align-center justify-center>
+      <h1>{{caption}}</h1>
+      <p>{{subtitle}}</p>
+    </v-layout>
+  </v-parallax>
+</template>
+
+<script>
+
+  export default {
+    data() {
+      return {
+      };
+    },
+    props: [
+      'src',
+      'caption',
+      'subtitle',
+    ],
+    name: 'FundamentJumbo',
+  };
+</script>
+<style scoped>
+h1:after {
+  content: '';
+  display: block;
+  width: 60px;
+  margin-left: calc(50% - 30px);
+  border-top: 4px solid #80cbc4!important;
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+
+.border-top {
+  border-top: 4px solid #80cbc4!important;
+}
+</style>
