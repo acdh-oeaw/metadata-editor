5692c71c9fd4d96c354a75328598a1c8d318a2c6
Christoph Hoffmann <Christoph.Hoffmann@oeaw.ac.at>
2018-04-30 16:42:57 +0200

    menu corrections

--- src/components/Fundament/FundamentNav.vue
+++ src/components/Fundament/FundamentNav.vue
@@ -1,77 +1,83 @@
 <template>
   <div>
     <transition :duration="200" name="slideLeft" mode="out-in">
-    <v-navigation-drawer
-      v-if="$store.state.app.drawer"
-      :mini-variant="$store.state.app.miniVariant"
-      v-model="drawer"
-      enable-resize-watcher
-      :class="$store.state.app.config.color"
-      dark
-      app
-      >
-      <v-container
-         fill-height
-         @mouseover.stop="setNavDrawerMaxi()"
-         @mouseleave.stop="setNavDrawerMini()"
-      >
-        <v-layout column justify-space-between>
-          <v-list>
-            <v-list-tile>
-              <v-btn icon @click.stop="toggleAppMode()">
-                <v-icon>view_day</v-icon>
-              </v-btn>
-              <v-btn icon @click.stop="toggleNavDrawerClipped()">
-                <v-icon v-html="$store.state.app.drawerclipped?'first_page':'last_page'" v-if="!$store.state.app.miniVariant"></v-icon>
-              </v-btn>
-            </v-list-tile>
-          </v-list>
-          <v-list>
-            <v-list-tile
-              value="true"
-              v-for="(item, i) in $store.state.app.config.menu"
-              :key="i"
-              :to="item.startpage"
-            >
-                <v-btn icon>
-                  <v-icon v-html="item.icon"></v-icon>
+      <v-navigation-drawer
+        v-if="$store.state.app.drawer"
+        :mini-variant="$store.state.app.miniVariant"
+        v-model="drawer"
+        enable-resize-watcher
+        :class="$store.state.app.config.color"
+        dark
+        app
+        >
+        <v-container
+           fill-height
+           @mouseover.stop="setNavDrawerMaxi()"
+           @mouseleave.stop="setNavDrawerMini()"
+        >
+          <v-layout column justify-space-between>
+            <v-list>
+              <v-list-tile>
+                <v-btn icon @click.stop="toggleAppMode()">
+                  <v-icon>view_day</v-icon>
                 </v-btn>
-              <v-list-tile-content>
-                <v-list-tile-title v-text="item.caption"></v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list>
-          <v-list>
-            <v-list-tile>
-            </v-list-tile>
-          </v-list>
-        </v-layout>
-      </v-container>
-    </v-navigation-drawer>
+                <v-btn icon @click.stop="toggleNavDrawerClipped()">
+                  <v-icon v-html="$store.state.app.drawerclipped?'first_page':'last_page'" v-if="!$store.state.app.miniVariant"></v-icon>
+                </v-btn>
+              </v-list-tile>
+            </v-list>
+            <v-list>
+              <v-list-tile
+                value="true"
+                v-for="(item, i) in $store.state.app.config.menu"
+                :key="i"
+                :to="item.startpage"
+              >
+                  <v-btn icon>
+                    <v-icon v-html="item.icon"></v-icon>
+                  </v-btn>
+                <v-list-tile-content >
+                  <v-list-tile-title color="white">{{item.caption}}</v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+            </v-list>
+            <v-list>
+              <v-list-tile>
+              </v-list-tile>
+            </v-list>
+          </v-layout>
+        </v-container>
+      </v-navigation-drawer>
     </transition>
     <transition :duration="200" name="slideRight" mode="out-in">
-    <v-toolbar
-      v-if="!$store.state.app.drawer"
-      app
-      height="90"
-      class="border-bottom"
-      >
-      <v-btn icon @click.stop="toggleAppMode()">
-        <v-icon>view_quilt</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="$store.state.app.config.title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn>
-    </v-toolbar>
+      <v-toolbar
+        v-if="!$store.state.app.drawer"
+        app
+        height="90"
+        class="border-bottom"
+        >
+        <v-btn icon @click.stop="toggleAppMode()">
+          <v-icon>view_quilt</v-icon>
+        </v-btn>
+        <v-avatar
+          size="35"
+          class="teal lighten-3"
+        >
+          <img :src="$store.state.app.config.logo" alt="alt">
+        </v-avatar>
+        <v-toolbar-title v-text="$store.state.app.config.title"></v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon @click.stop="rightDrawer = !rightDrawer">
+          <v-icon>menu</v-icon>
+        </v-btn>
+      </v-toolbar>
     </transition>
     <v-navigation-drawer
       :right="true"
       v-model="$store.state.app.rightDrawer"
       enable-resize-watcher
       app
-    >
+      >
       <v-list>
         <v-list-tile @click="right = !right">
           <v-list-tile-action>
