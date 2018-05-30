7677fc60ffdfe6656b20a1238527dc10f4d9a48f
Lukas Moertl <lukas.moertl@gmx.at>
2018-05-30 16:41:33 +0200

    added a few types. on trying to remove duplicates i found that in FormFromShema line 52 and 53 are in javascript's opinion identical.

--- src/components/FormFromSchema.vue
+++ src/components/FormFromSchema.vue
@@ -30,17 +30,66 @@ const TYPES = [
   'Organisation',
   'PublicationOrRepoObject',
   'CollectionOrResourceOrPublication',
-  'PlaceOrPublicationOrRepoObject',
   'RepoObject',
+  'Metadata',
   'anyURI',
   'date',
   'string',
   'text',
   'positiveInteger',
+  'CollectionOrPlaceOrPublicationOrResource',
+  '​CollectionOrPlaceOrPublicationOrResource',
+  'Agent',
+  '​AgentOrPlace',
+  'CollectionOrResource',
+  'CollectionOrResourceOrPublication',
+  '​ContainerOrReMe',
+  '​ContainerOrResource',
+  '​Image',
+  '​Main',
+  '​Metadata',
+  '​Place',
+  '​PlaceOrPublicationOrRepoObject',
+  'PlaceOrPublicationOrRepoObject',
+  '​Project',
+  '​Publication',
+  '​PublicationOrRepoObject',
+  '​anyURI',
+  'date',
+  '​positiveInteger',
+  '​string',
 ];
 
-console.log('types in formfromshema', TYPES);
+/*
+Agent: 15
+​AgentOrPlace: 1
+​CollectionOrPlaceOrPublicationOrResource: 1
+CollectionOrResource: 1
+​CollectionOrResourceOrPublication: 2
+​ContainerOrReMe: 1
+​ContainerOrResource: 2
+​Image: 1
+​Main: 1
+​Metadata: 1
+​Place: 1
+​PlaceOrPublicationOrRepoObject: 1
+​Project: 1
+​Publication: 1
+​PublicationOrRepoObject: 2
+​anyURI: 7
+​date: 13
+​positiveInteger: 2
+​string: 24
+*/
 
+
+let unique = [];
+
+for (let t = 0; t < TYPES.length; t += 1) {
+  if (unique.indexOf(TYPES[t]) === -1)
+    unique.push(TYPES[t]);
+}
+console.log('types in formfromshema', unique);
 
 for (let i = 0; i < TYPES.length; i += 1) {
   const t = TYPES[i];
