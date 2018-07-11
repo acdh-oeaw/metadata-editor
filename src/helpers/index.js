edb3d25b1654e24b47c37d499efb3b7448fb2138
Lukas Moertl <lukas.moertl@gmx.at>
2018-07-11 13:39:34 +0200

    eslint fix

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -189,7 +189,7 @@ export default {
     isIdentifier(id) {
       this.$debug('isIdentifier, id, APIS.ARCHE2.ID', id, APIS.ARCHE2.ID);
       if (id) {
-        const identi = escape(id)
+        const identi = escape(id);
         return APIS.ARCHE2.ID.get(`${identi}`).then((response) => {
           this.$log('   good response', response.data);
           return Promise.resolve(response.data.title ? response.data : false);
